8aed75f55592d8c20b2d0cc4cacc65df40f9274e
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2019-11-02 14:41:24 -0300

    [DEV] initial project
    Signed-off-by: Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>

---
+++ src/main/java/com/joseyustiz/springsecurityjpajwt/SpringSecurityJpaJwtApplication.java
@@ -0,0 +1,13 @@
+package com.joseyustiz.springsecurityjpajwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJpaJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJpaJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joseyustiz/springsecurityjpajwt/SpringSecurityJpaJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseyustiz.springsecurityjpajwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJpaJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
