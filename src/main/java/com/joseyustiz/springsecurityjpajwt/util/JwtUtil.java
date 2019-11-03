ebf46996c357a993d1c3f6a83cf7c5600c19242d
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2019-11-03 18:28:53 -0300

    [DEV] Moved jwt secret to the application.properties
    Signed-off-by: Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>

--- src/main/java/com/joseyustiz/springsecurityjpajwt/util/JwtUtil.java
+++ src/main/java/com/joseyustiz/springsecurityjpajwt/util/JwtUtil.java
@@ -3,6 +3,7 @@ package com.joseyustiz.springsecurityjpajwt.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +18,9 @@ public class JwtUtil {
     private static final int TEN_HOURS = 10;
     private static final int SIXTY_MINS = 60;
     private static final int SIXTY_SECS = 60;
-    private static final int ONE_MILISECS = 1000;
-    private String SECRET_KEY = "secret";
+    private static final int ONE_MILLISECS = 1000;
+    @Value("${jwt.secret}")
+    private String secretKey;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -34,7 +36,7 @@ public class JwtUtil {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
     }
 
     private Boolean isTokenExpired(String token) {
@@ -49,8 +51,8 @@ public class JwtUtil {
     private String createToken(Map<String, Object> claims, String subject) {
 
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + ONE_MILISECS * SIXTY_SECS * SIXTY_MINS * TEN_HOURS))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+                .setExpiration(new Date(System.currentTimeMillis() + ONE_MILLISECS * SIXTY_SECS * SIXTY_MINS * TEN_HOURS))
+                .signWith(SignatureAlgorithm.HS256, secretKey).compact();
     }
 
     public Boolean validateToken(String token, UserDetails userDetails) {
