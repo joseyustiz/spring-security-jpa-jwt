0566563b4644d573dbc1818f22836e794ad49eec
Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>
2019-11-03 11:12:37 -0300

    [DEV] Return HTTP Status 201 for successful signUp
    Signed-off-by: Jose Angel Yustiz Jimenez <jose.yustiz@gmail.com>

--- src/main/java/com/joseyustiz/springsecurityjpajwt/web/UserController.java
+++ src/main/java/com/joseyustiz/springsecurityjpajwt/web/UserController.java
@@ -6,7 +6,9 @@ import com.joseyustiz.springsecurityjpajwt.model.exception.EmailUsedException;
 import com.joseyustiz.springsecurityjpajwt.service.UserSignUpService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,11 +25,11 @@ public class UserController {
     private final UserSignUpService userSignUpService;
 
     @PostMapping(value = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public RegisteredUser signUp(@RequestBody @Valid SignUpRequest signUpRequest) throws EmailUsedException {
+    public ResponseEntity<RegisteredUser> signUp(@RequestBody @Valid SignUpRequest signUpRequest) throws EmailUsedException {
         log.info("Request - signUpRequest=[{}]", signUpRequest);
         RegisteredUser registeredUser = userSignUpService.signUp(signUpRequest);
         log.info("Response - registeredUser=[{}]", registeredUser);
-        return registeredUser;
+        return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
     }
 
 }
